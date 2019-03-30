f6bb432e193ef346b4d596115295cb7766a59fae
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2019-03-30 17:00:44 +0200

    adding return url

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -8,11 +8,11 @@ export class AuthGuard implements CanActivate {
         private authService: AuthService,
         private router: Router
     ) { }
-    canActivate(): boolean {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         if (this.authService.isAuthenticated()) {
             return true;
         }
-        this.router.navigate(['/register']);
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
         return false;
     }
 }
