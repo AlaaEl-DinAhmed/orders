9bce54bb10decf58552f2669aca031c550825d9f
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2019-04-05 15:33:17 +0200

    adding http-interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/header/header.component';
+import { FooterComponent } from './app/footer/footer.component';
+import { HomeComponent } from './app/core/home/home.component';
 import { LoginComponent } from './app/core/login/login.component';
+import { RegisterComponent } from './app/core/register/register.component';
 import { NotFoundComponent } from './app/core/not-found/not-found.component';
 import { AuthService } from './services/auth.service';
-import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './app/core/home/home.component';
-import { RegisterComponent } from './app/core/register/register.component';
-import { FooterComponent } from './app/footer/footer.component';
-import { HeaderComponent } from './app/header/header.component';
 import { AuthGuard } from './services/auth-guard.service';
+import { AuthInterceptor } from './services/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,15 @@ import { AuthGuard } from './services/auth-guard.service';
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [AuthService, AuthGuard],
+  providers: [
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpInterceptor, HttpHandler, HttpRequest
+} from '@angular/common/http';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const clonedReq = req.clone({
+            headers: req.headers.append('Authorization', `Bearer ${this.authService.getToken()}`)
+        });
+        return next.handle(clonedReq);
+    }
+}
